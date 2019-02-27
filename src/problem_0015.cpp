281bcb8d30e5839b26ebe9d7672f0fd1bea471ae
guicaisa <guicaisa@163.com>
2019-02-27 18:45:54 +0800

    problem 15 16

---
+++ src/problem_0015.cpp
@@ -0,0 +1,87 @@
+﻿#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+/**
+ * https://leetcode.com/problems/3sum/
+ */
+
+class Solution {
+public:
+	/**
+	 * 排序数组，遍历每个数字，在其之后的区间内，查找与其相加等于0的两个数
+	 * 根据结果大小左移或者右移区间，在区间内去重，保存结果
+	 * 同时在外层循环中去除相同的数字，防止重复计算结果
+	 */
+	std::vector<std::vector<int> > ThreeSum(std::vector<int>& nums) 
+	{
+		std::vector<std::vector<int> > result;
+
+		if (nums.size() < 3)
+		{
+			return result;
+		}
+
+		std::sort(nums.begin(), nums.end());
+		
+		for (size_t i = 0; i < nums.size(); ++i)
+		{
+			if (i > 0 && nums[i] == nums[i - 1])
+			{
+				continue;
+			}
+
+			int left = i + 1;
+			int right = nums.size() - 1;
+
+			while (left < right)
+			{
+				int sum = nums[i] + nums[left] + nums[right];
+
+				if (sum < 0)
+				{
+					++left;
+				}
+				else if (sum > 0)
+				{
+					--right;
+				}
+				else
+				{
+					std::vector<int> temp_vec;
+					temp_vec.emplace_back(nums[i]);
+					temp_vec.emplace_back(nums[left]);
+					temp_vec.emplace_back(nums[right]);
+					result.push_back(temp_vec);
+
+					while (left + 1 < nums.size() && nums[left] == nums[left + 1])
+					{
+						++left;
+					}
+
+					while (right - 1 >= 0 && nums[right] == nums[right - 1])
+					{
+						--right;
+					}
+
+					++left;
+					--right;
+				}
+			}
+		}
+
+		return result;
+	}
+};
+
+//int main(int argc, char** argv)
+//{
+//	Solution s;
+//
+//	std::vector<int> nums = { -1,0,1,2,-1,-4};
+//
+//	std::vector<std::vector<int> > result = s.ThreeSum(nums);
+//
+//
+//	return 0;
+//}
---
+++ src/problem_0016.cpp
@@ -0,0 +1,89 @@
+﻿#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+/**
+ * https://leetcode.com/problems/3sum-closest/
+ */
+
+class Solution 
+{
+public:
+	/**
+	 * 解法与3sum类似，先排序数组，使用左右边界，只有在外层循环需要去重
+	 * 根据3个数的和与目标数比较是大还是小，如果大就将右边界往左移
+	 * 如果小，就将左边界往右移，相同就直接返回，有且只有一个解
+	 * 内层不需要判断去重
+	 */
+	int ThreeSumClosest(std::vector<int>& nums, const int target) 
+	{
+		if (nums.size() == 0)
+		{
+			return 0;
+		}
+
+		sort(nums.begin(), nums.end());
+
+		int min_diff = INT_MAX;
+		int closest_sum = 0;
+
+		for (size_t i = 0; i < nums.size(); ++i)
+		{
+			if (i > 0 && nums[i] == nums[i - 1])
+			{
+				continue;
+			}
+
+			int left = i + 1;
+			int right = nums.size() - 1;
+			int temp_min_diff = INT_MAX;
+			int temp_closest_sum = 0;
+
+			while (left < right)
+			{
+				int sum = nums[i] + nums[left] + nums[right];
+
+				if (abs(sum - target) < temp_min_diff)
+				{
+					temp_closest_sum = sum;
+					temp_min_diff = abs(sum - target);
+				}
+
+				if (sum < target)
+				{
+					++left;
+				}
+				else if (sum > target)
+				{
+					--right;
+				}
+				else
+				{
+					return sum;
+				}
+
+				if (temp_min_diff < min_diff)
+				{
+					min_diff = temp_min_diff;
+					closest_sum = temp_closest_sum;
+				}
+			}
+		}
+
+		return closest_sum;
+	}
+};
+
+//int main(int argc, char** argv)
+//{
+//	Solution s;
+//
+//	std::vector<int> vec = { -1, 0, 1, 1, 55 };
+//
+//
+//	int ret = s.ThreeSumClosest(vec, 3);
+//
+//
+//
+//	return 0;
+//}
