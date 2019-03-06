98843a10a791eef6d636e5d5d1a5ed8b5c5a6bd0
guicaisa <guicaisa@163.com>
2019-03-06 19:46:45 +0800

    problem 20 21

---
+++ src/problem_0020.cpp
@@ -0,0 +1,75 @@
+﻿#include <stdio.h>
+#include <string>
+#include <stack>
+
+/**
+ * https://leetcode.com/problems/valid-parentheses/
+ */
+
+class Solution 
+{
+public:
+	/**
+	 * 考虑括号的对称性结果，使用栈的方式，遇到左括号入栈
+	 * 遇到右括号时查看栈顶元素，如果无元素或者不是对应的左括号则不是有效的括号结构
+	 */
+	bool IsValid(const std::string& s)
+	{
+		std::stack<char> char_st;
+
+		for (size_t i = 0; i < s.size(); ++i)
+		{
+			switch (s[i])
+			{
+				case '(':
+				case '[':
+				case '{':
+					char_st.push(s[i]);
+					break;
+				case ')':
+					if (char_st.size() == 0 || char_st.top() != '(')
+					{
+						return false;
+					}
+					else
+					{
+						char_st.pop();
+					}
+					break;
+				case ']':
+					if (char_st.size() == 0 || char_st.top() != '[')
+					{
+						return false;
+					}
+					else
+					{
+						char_st.pop();
+					}
+					break;
+				case '}':
+					if (char_st.size() == 0 || char_st.top() != '{')
+					{
+						return false;
+					}
+					else
+					{
+						char_st.pop();
+					}
+					break;
+				default:
+					break;
+			}
+		}
+
+		return char_st.size() == 0;
+	}
+};
+
+//int main(int argc, char** argv)
+//{
+//	Solution s;
+//
+//	bool ret = s.IsValid("{[]}");
+//
+//	return 0;
+//}
---
+++ src/problem_0021.cpp
@@ -0,0 +1,110 @@
+﻿#include <stdio.h>
+
+/**
+ * https://leetcode.com/problems/merge-two-sorted-lists/
+ */
+
+struct ListNode 
+{
+	ListNode(int x)
+		: val(x),
+		next(NULL)
+	{
+	}
+
+    int val;
+    ListNode* next;
+};
+
+class Solution 
+{
+public:
+	/**
+	 * 遍历2个指针，根据值域的大小安排其节点在新链表中的位置
+	 * 如果一个链表遍历到底了，就直接将还没遍历完的链表直接接在新链表的后面
+	 */
+	ListNode* MergeTwoLists(ListNode* l1, ListNode* l2)
+	{
+		ListNode* result = NULL;
+		ListNode* cur_node = NULL;
+
+		while (l1 || l2)
+		{
+			ListNode* temp = NULL;
+
+			if (l1 && l2)
+			{
+				if (l1->val < l2->val)
+				{
+					temp = l1;
+					l1 = l1->next;
+				}
+				else
+				{
+					temp = l2;
+					l2 = l2->next;
+				}
+			}
+			else if (l1)
+			{
+				if (result)
+				{
+					cur_node->next = l1;
+				}
+				else
+				{
+					result = l1;
+				}
+				break;
+
+			}
+			else if (l2)
+			{
+				if (result)
+				{
+					cur_node->next = l2;
+				}
+				else
+				{
+					result = l2;
+				}
+				break;
+			}
+
+			temp->next = NULL;
+
+			if (result == NULL)
+			{
+				result = temp;
+			}
+			else
+			{
+				cur_node->next = temp;
+			}
+			cur_node = temp;
+		}
+
+		return result;
+	}
+};
+
+//int main(int argc, char** argv)
+//{
+//	Solution s;
+//
+//	ListNode* node_1_1 = new ListNode(-9);
+//	ListNode* node_1_2 = new ListNode(3);
+//	ListNode* node_1_3 = new ListNode(4);
+//	node_1_1->next = node_1_2;
+//	//node_1_2->next = node_1_3;
+//
+//	ListNode* node_2_1 = new ListNode(5);
+//	ListNode* node_2_2 = new ListNode(7);
+//	ListNode* node_2_3 = new ListNode(4);
+//	node_2_1->next = node_2_2;
+//	//node_2_2->next = node_2_3;
+//
+//	ListNode* ret = s.MergeTwoLists(node_1_1, node_2_1);
+//
+//	return 0;
+//}
