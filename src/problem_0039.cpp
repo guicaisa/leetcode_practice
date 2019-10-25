c871375ab88f1a60c57fb17d15c70a7c88472b65
guicaisa <guicaisa@163.com>
2019-10-25 18:00:36 +0800

    problem 39

---
+++ src/problem_0039.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <algorithm>
+
+/**
+ * https://leetcode.com/problems/combination-sum/
+ */
+
+class Solution 
+{
+public:
+	// 递归处理所有的情况
+	std::vector<std::vector<int> > CombinationSum(const std::vector<int>& candidates, const int target) 
+	{
+		sort(candidates.begin(), candidates.end());
+		std::vector<int> temp;
+		std::vector<std::vector<int> > result;
+
+		if (target == 0)
+		{
+			return result;
+		}
+
+		Recursive(candidates, target, 0, temp, result);
+
+		return result;
+	}
+
+	void Recursive(const std::vector<int>& candidates, const int target, const int index, std::vector<int>& temp, 
+		std::vector<std::vector<int> >& result)
+	{
+		if (target == 0)
+		{
+			result.push_back(temp);
+		}
+
+		for (int i = index; i < candidates.size() && target - candidates[i] >= 0; ++i)
+		{
+			temp.push_back(candidates[i]);
+			Recursive(candidates, target - candidates[i], i, temp, result);
+			temp.pop_back();
+		}
+	}
+};
+
+//int main(int argc, char** argv)
+//{
+//	Solution s;
+//
+//	std::vector<int> vi = { 2, 3, 5 };
+//
+//	std::vector<std::vector<int> > result = s.CombinationSum(vi, 8);
+//
+//	return 0;
+//}
