21b15b48a529d5b35953941d13e973a3f7d4f15f
guicaisa <guicaisa@163.com>
2019-10-31 20:19:54 +0800

    problem 41 42

---
+++ src/problem_0042.cpp
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+
+/**
+ * https://leetcode.com/problems/trapping-rain-water/solution/
+ */
+
+class Solution 
+{
+public:
+	/**
+	 * 左右分别遍历，得到从左到右还有从右到左遍历时每个位置上的最高值 
+	 * 遍历数组，根据左右数组中的最高值小的一方位基准(木桶短板)
+	 * 获得与当前位置高度的差值，累计到结果中
+	 */
+	int Trap(const std::vector<int>& height)
+	{
+		if (height.size() == 0)
+		{
+			return 0;
+		}
+
+		std::vector<int> left(height.size(), 0);
+		std::vector<int> right(height.size(), 0);
+
+		left[0] = height[0];
+		right[height.size() - 1] = height[height.size() - 1];
+
+		for (int i = 1; i < height.size(); ++i)
+		{
+			left[i] = height[i] > left[i - 1] ? height[i] : left[i - 1];
+		}
+
+		for (int i = height.size() - 2; i >= 0; --i)
+		{
+			right[i] = height[i] > right[i + 1] ? height[i] : right[i + 1];
+		}
+
+		int sum = 0;
+
+		for (int i = 1; i < height.size() - 1; ++i)
+		{
+			sum += (left[i] < right[i] ? left[i] : right[i]) - height[i];
+		}
+
+		return sum;
+	}
+};
+
+int main(int argc, char** argv)
+{
+	Solution s;
+
+	std::vector<int> vi = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+
+	int ret = s.Trap(vi);
+
+	return 0;
+}
