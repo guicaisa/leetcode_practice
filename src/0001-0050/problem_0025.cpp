f7ae22c8f7a98699c70241ea5935684a52b5f4c8
guicaisa <guicaisa@163.com>
2021-04-20 22:48:13 +0800

    edit

--- src/0001-0050/problem_0025.cpp
+++ src/0001-0050/problem_0025.cpp
@@ -1,8 +1,6 @@
 ﻿#include <stdio.h>
 
-/**
- * https://leetcode.com/problems/reverse-nodes-in-k-group/
- */
+// https://leetcode-cn.com/problems/reverse-nodes-in-k-group/
 
 struct ListNode 
 {
@@ -19,99 +17,90 @@ struct ListNode
 class Solution 
 {
 public:
-	/**
-	 * 使用二级指针保存头部节点指针的位置，用来更新置换顺序之后的头部节点
-	 * 使用计数变量，在遍历每k个节点之后，倒置该k个节点的顺序，将当前节点
-	 * 即倒置之后的头结点赋值给二级指针的解引用，达到替换头节点的目的
-	 * 将之前的头节点，即倒置之后的尾节点的next域的指针重新赋值给二级指针
-	 * 在下一次遍历过程中作为下一组节点连接的头节点，倒置节点使用的是递归的方式
-	 */
+	// 使用一个多余的头指针来保存最终的结果，使用一个pre指针来辅助处理连续k个节点交换顺序
+	// 遍历链表，使用计数变量，在遍历每k个节点之后，保存尾节点的next指针，倒置该k个节点的顺序
+	// 将原来的头节点，即现在的尾节点的next设置为之前保存的next
+	// 将原来的尾节点，即现在的头节点放在pre指针的next域，即使完成k个节点的交换顺序
+	// 并将倒序后的节点重新接入已有的链表中
 	ListNode* ReverseKGroup(ListNode* head, int k)
 	{
-		ListNode** p = &head;
-		ListNode* front_node = head;
-		ListNode* cur_node = head;
+		ListNode* n = new ListNode(-1);
+		n->next = head;
+		ListNode* pre = n;
+		ListNode* cur = head;
 		int count = 0;
-
-		while (cur_node)
+		
+		while (cur)
 		{
 			++count;
-
 			if (count == k)
 			{
-				//保存下一段节点的开头
-				ListNode* next_node = cur_node->next;
-				cur_node->next = NULL;
+				// 此处cur为k个节点中的尾节点，保存next
+				ListNode* next = cur->next;
+				cur->next = NULL;
+				// 此处first为k个节点中的头节点
+				ListNode* first = pre->next;
+				// 倒置顺序
+				this->ReverseNode(first);
+				// 此处的first已经变为尾节点，将保存的next赋值给其next域
+				first->next = next;
+				// 此处的cur已经变为头节点，将其赋值给pre的next域
+				pre->next = cur;
 
-				ListNode* tail_node = this->ReverseNode(front_node);
-				//当前节点已经是头节点了
-				*p = cur_node;
-				//尾节点就是倒置之前的头节点，其next域作为下一段节点的连接头
-				p = &tail_node->next;
-
-				//重置变量，开启新一轮遍历计数
-				cur_node = next_node;
-				front_node = next_node;
+				// 当前的尾节点作为下一轮k个节点的pre节点
+				pre = first;
+				cur = pre->next;
 				count = 0;
 			}
 			else
 			{
-				cur_node = cur_node->next;
+				cur = cur->next;
 			}
 		}
 
-		if (count > 0 && count < k)
-		{
-			*p = front_node;
-		}
-
-		return head;
+		return n->next;
 	}
 
-	/**
-	 * 与第一个函数功能相同，只是把倒置节点的函数从递归改成了循环，
-	 * 使用二级指针保存头节点的位置，用赋值解引用的方式完成头节点的替换，耗时会低一些
-	 */
-	ListNode* ReverseKGroupBetter(ListNode* head, int k) 
+	// 与第一个方法是相同的思路，只不过把中间倒顺链表的方式从递归改成了遍历
+	ListNode* ReverseKGroupIterate(ListNode* head, int k)
 	{
-		ListNode** p = &head;
-		ListNode* front_node = head;
-		ListNode* cur_node = head;
+		ListNode* n = new ListNode(-1);
+		n->next = head;
+		ListNode* pre = n;
+		ListNode* cur = head;
 		int count = 0;
 
-		while (cur_node)
+		while (cur)
 		{
 			++count;
-
-			if (count == k)
-			{
-				//保存下一段节点的开头
-				ListNode* next_node = cur_node->next;
-				cur_node->next = NULL;
-
-				this->ReverseNodeOth(front_node, k);
-				//当前节点已经是头节点了
-				*p = cur_node;
-				//头节点在倒置之后已经成为了尾节点，其next域作为下一段节点的连接头
-				p = &front_node->next;
-
-				//重置变量，开启新一轮遍历计数
-				cur_node = next_node;
-				front_node = next_node;
-				count = 0;
-			}
-			else
-			{
-				cur_node = cur_node->next;
-			}
+			cur = cur->next;
 		}
 
-		if (count > 0 && count < k)
+		cur = head;
+		while (count >= k)
 		{
-			*p = front_node;
+			for (int i = 1; i < k; ++i)
+			{
+				// 这里的cur永远是k个节点中的第一个节点
+				// 整个循环不断把cur往后移
+				// cur的next即下一个要调到头部的节点
+				ListNode* next = cur->next;
+				// cur的next变成后面隔一个节点
+				cur->next = next->next;
+				// next即将被放在k个节点的头部，将当前的头部节点当作自己的next
+				next->next = pre->next;
+				// 将next放在k个节点的第一个位置上
+				pre->next = next;
+			}
+
+			// 下一轮
+			pre = cur;
+			cur = pre->next;
+
+			count -= k;
 		}
 
-		return head;
+		return n->next;
 	}
 
 private:
@@ -127,25 +116,6 @@ private:
 		node->next = NULL;
 
 		return node;
-	}
-
-	ListNode* ReverseNodeOth(ListNode* head, int k)
-	{
-		ListNode** p_head = &head;
-		ListNode* cur_node = head;
-
-		for (int i = 1; i < k; ++i)
-		{
-			ListNode* next_node = cur_node->next;
-			ListNode* temp_node = next_node->next;
-
-			next_node->next = *p_head;
-			cur_node->next = temp_node;
-
-			*p_head = next_node;
-		}
-
-		return head;
 	}
 };
 
@@ -165,7 +135,7 @@ private:
 //	node_4->next = node_5;
 //
 //
-//	ListNode* ret = s.ReverseKGroup(node_1, 3);
+//	ListNode* ret = s.ReverseKGroupIterate(node_1, 2);
 //
 //	return 0;
 //}
