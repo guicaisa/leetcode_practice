bd1cae46550dc6c850e371a44a45b558c2394848
guicaisa <guicaisa@163.com>
2021-05-09 15:28:20 +0800

    edit

--- src/0001-0050/problem_0030.cpp
+++ src/0001-0050/problem_0030.cpp
@@ -3,59 +3,58 @@
 #include <unordered_map>
 #include <vector>
 
-/**
- * https://leetcode.com/problems/substring-with-concatenation-of-all-words/
- */
+// https://leetcode-cn.com/problems/substring-with-concatenation-of-all-words/
 
 class Solution
 {
 public:
+	// words中的所有字符串都是等长的，所以当我们在s中寻找串联的子串的时候
+	// 可以使用一个固定的步幅single_len来处理字符串
+	// 我们使用一个map来保存words中所有字符出现的次数
+	// total_len代表words中所有字符构成的子串的总长度
+	// 遍历s字符串，以每个字符为起点，以single_len的长度获取每个字符
+	// 使用临时的map来保存这些字符出现的次数，并且与之前的map中已保存的字符出现的次数进行对比
+	// 如果个数相符，则找到合适的子串起始位置，如不相符，则继续下一次循环处理
 	std::vector<int> FindSubString(const std::string& s, const std::vector<std::string>& words) 
 	{
 		std::vector<int> vi;
-		std::unordered_map<std::string, int> sm;
-
+		
 		if (s == "" || words.size() == 0)
 		{
 			return vi;
 		}
 
-		for (int i = 0; i < words.size(); ++i)
+		size_t single_len = words[0].size();
+		size_t total_len = single_len * words.size();
+		size_t s_len = s.size();
+		std::unordered_map<std::string, int> words_map;
+
+		for (size_t i = 0; i < words.size(); ++i)
 		{
-			++sm[words[i]];
+			words_map[words[i]] += 1;
 		}
 
-		int p1 = 0;
-		int p2 = 0;
-		int len = words[0].size();
-		int count = words.size();
-		int n = s.size();
-
-		for (; p1 < n - (count * len) + 1; ++p1)
+		for (size_t i = 0; i < s_len && s_len - i >= total_len; ++i)
 		{
-			std::unordered_map<std::string, int> tsm;
-
-			p2 = 0;
-			for (; p2 < count; ++p2)
+			bool flag = true;
+			std::unordered_map<std::string, int> temp_words_map;
+			for (size_t j = 0; j < words.size(); ++j)
 			{
-				std::string ts = s.substr(p1 + (p2 * len), len);
-				if (sm.find(ts) != sm.end())
+				std::string word = s.substr(i + j * single_len, single_len);
+				if (temp_words_map[word] < words_map[word])
 				{
-					++tsm[ts];
-					if (tsm[ts] > sm[ts])
-					{
-						break;
-					}
-				} 
+					temp_words_map[word] += 1;
+				}
 				else
 				{
+					flag = false;
 					break;
 				}
 			}
 
-			if (p2 == count)
+			if (flag)
 			{
-				vi.push_back(p1);
+				vi.emplace_back(i);
 			}
 		}
 
