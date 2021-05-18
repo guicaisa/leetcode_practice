97125b0f3b1eda22772f9f202970e62a0469240c
guicaisa <guicaisa@163.com>
2021-05-18 22:06:14 +0800

    eidt

--- src/0001-0050/problem_0031.cpp
+++ src/0001-0050/problem_0031.cpp
@@ -1,14 +1,25 @@
+#include <iostream>
 #include <stdio.h>
 #include <vector>
 #include <map>
+#include <algorithm>
 
-/**
- * https://leetcode.com/problems/next-permutation/
- */
+// https://leetcode-cn.com/problems/next-permutation/
 
 class Solution
 {
 public:
+	// 根据肉眼观察，寻找下一个数字排列，需要从数组的右边开始寻找第一组
+	// 是升序形式的2个数字，将其进行交换才能使得下一次的数字排列有可能是更大的
+	// 比如1，2，3，4，从最右边开始找到的第一个升序排列的2个数字是3，4，交换3和4
+	// 获得下一个数字排列，1，2，4，3
+	// 如果是另一种复杂的情况，比如1，4，3，2，从右开始找到第一组升序排列的数字是1，4
+	// 这里不能直接交换1和4的位置，因为4后面还有其他比4小的数字，如果是4，1，3，2的话明显
+	// 结果是不对的，因为中间还有很多次大一点的排列没有列举出来
+	// 遇到这种情况，应该从尾遍历到1之前的所有数字，遇到第一个比1大的数字，就交换这个数字和1的位置
+	// 因为1后面的数字都可以确定是降序排列的，所以这次交换可以保证比1大的最小的一个数字会出现在1的位置上
+	// 保证了下一个数字排列的正确性
+	// 再将后续的3个数字的顺序倒置，保证后面3个数字的排列顺序是最小的原始的升序状态
 	void NextPermutation(std::vector<int>& nums)
 	{
 		if (nums.size() == 0)
@@ -16,54 +27,21 @@ public:
 			return;
 		}
 
-		std::map<int, int> greater;
-		std::map<int, int> less;
-
-		// 从后往前，找到第一个降序的数字，作为替换节点
-		// 从该数字的后一个数字开始直到结尾，保存一部分大于其的数字和一部分小于其的数字
-		// 并且将该部分的所有数字从数组中剔除
-		// 从大于该数字的集合中找到最小的一个替换到该位置上去
-		// 剩下的数字全部放在一个集合中，根据大小升序依次补位到原数组中
-		for (int i = nums.size() - 1; i >= 1; --i)
+		for (int i = nums.size() - 2; i >= 0; --i)
 		{
-			if (nums[i] > nums[i - 1]) 
+			if (nums[i] < nums[i + 1])
 			{
-				int t = nums[i - 1];
-				for (int j = i; j < nums.size(); ++j)
+				for (int j = nums.size() - 1; j > i; --j)
 				{
-					if (nums[j] > t)
+					if (nums[j] > nums[i])
 					{
-						++greater[nums[j]];
-					}
-					else
-					{
-						++less[nums[j]];
-					}
-				}
-
-				nums.erase(nums.begin() + i - 1, nums.end());
-
-				nums.emplace_back(greater.begin()->first);
-				if (--(greater.begin()->second) == 0)
-				{
-					greater.erase(greater.begin()->first);
-				}
-			
-				++greater[t];
-				for (std::map<int, int>::iterator it = less.begin(); it != less.end(); ++it)
-				{
-					greater[it->first] += it->second;
-				}
-
-				for (std::map<int, int>::iterator it = greater.begin(); it != greater.end(); ++it)
-				{
-					for (int j = 0; j < it->second; ++j)
-					{
-						nums.emplace_back(it->first);
+						int temp = nums[j];
+						nums[j] = nums[i];
+						nums[i] = temp;
+						std::reverse(nums.begin() + i + 1, nums.end());
+						return;
 					}
 				}
-
-				return;
 			}
 		}
 
@@ -75,10 +53,12 @@ public:
 //{
 //	Solution s;
 //
-//	std::vector<int> vi = {1, 3, 2};
+//	std::vector<int> vi = {1,4,3,2};
 //	// 
 //
 //	s.NextPermutation(vi);
+//
+//	std::cout << vi[0] << vi[1] << vi[2] << vi[3] << std::endl;
 //
 //	return 0;
 //}
