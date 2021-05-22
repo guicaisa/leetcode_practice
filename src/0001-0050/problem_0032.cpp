e56ebed9da5f25971add7bd23cffe59468862c11
guicaisa <guicaisa@163.com>
2021-05-22 22:12:13 +0800

    edit

--- src/0001-0050/problem_0032.cpp
+++ src/0001-0050/problem_0032.cpp
@@ -4,13 +4,15 @@
 #include <stack>
 #include <map>
 
-/**
- * https://leetcode.com/problems/longest-valid-parentheses/
- */
+// https://leetcode-cn.com/problems/longest-valid-parentheses/
 
 class Solution {
 public:
 	// 动态规划 dp数组中dp[i]代表s[0...i]的字串中最长的有效括号长度
+	// 这个dp有个限制，就是如果在当前的s[0...i]中，以0...i-1任意一个字符为起始，以i为结尾的字串
+	// 必须是有效的括号，才计算其的长度dp[i]，否则计为0，比如s[i]为'('，那么s[0..i]中的任意字串都不可能是有效的括号
+	// dp[i]为0，这是为了当遇到有效的括号可以延续长度的时候可以方便使用dp的既有结果做计算
+	// 如果中间括号的有效性断掉的话，就得重新计算有效括号的字串的长度
 	// 根据括号的特性，有2种情况
 	// 1. 当s[i] == ')' 并且 s[i - 1] == '('时，s[i-1...i]之间的有效括号长度为2
 	// 此时判断s[0...s-2]之间的有效括号长度，即dp[i - 2]，在此基础上+2，即得到公式
@@ -56,8 +58,9 @@ public:
 	}
 
 	// 栈的方法
-	// 保持栈底一直有一个标识起始位置索引的值，'('入栈，')'出栈
-	// 出栈时根据该索引计算有效括号的长度
+	// 保持栈底一直有一个标识起始位置之前索引的值，'('入栈，')'出栈
+	// 每个右括号出栈的时候，都可以查看栈中是否包含之前的索引位置
+	// 用以计算当前的有效括号的长度，并更新到最大长度中
 	int LongestValidParenthesesStack(const std::string& s)
 	{
 		int max_len = 0;
@@ -96,6 +99,9 @@ public:
 	}
 
 	// 左右遍历，根据'('和')'出现配对的情况进行判断
+	// 由于这次的括号只包含小括号，我们可以使用左右括号的变量计数来判断是否是合理的括号
+	// 注意，从左遍历的话右括号数大于左括号数，就代表当前的括号无效了，必须从头重新判断括号
+	// 反之亦然
 	int LongestValidParenthesesThrough(const std::string& s)
 	{
 		int max_len = 0;
@@ -159,7 +165,7 @@ public:
 //
 //	std::string ss = "())((())";
 //
-//	int ret = s.LongestValidParenthesesStack(ss);
+//	int ret = s.LongestValidParentheses(ss);
 //
 //
 //	return 0;
