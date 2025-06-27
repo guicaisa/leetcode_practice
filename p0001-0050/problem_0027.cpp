8019f3eb6885e115de98f37b1cd612b8f9a521bc
guicaisa <guicaisa@163.com>
2025-06-27 23:50:01 +0800

    modify style

--- p0001-0050/problem_0027.cpp
+++ p0001-0050/problem_0027.cpp
@@ -8,8 +8,9 @@ class Solution {
 public:
     //1. 双指针
     //使用一个单独的指针指向数组头部，然后用另外一个指针遍历数组，遇到与val不同的值则根据第一个指针赋值到数组中
-    //并指向下一个位置，否则就略过，数组遍历结束后，前n个元素即所有不等于valu的元素
-    int removeElement(vector<int>& nums, int val) {
+    //并指向下一个位置，否则就略过，数组遍历结束后，前n个元素即所有不等于val的元素
+    int removeElement(vector<int>& nums, int val) 
+    {
         int n = 0;
         for (int i = 0; i < nums.size(); ++i)
         {
